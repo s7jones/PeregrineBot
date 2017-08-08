29c71fb210157019873036e9b4999aee22b0e218
s7jones <s7jones@users.noreply.github.com>
2017-08-09 00:07:36 +0100

    Move Base tasks to BaseManager

--- PeregrineBot/Source/BaseManager.cpp
+++ PeregrineBot/Source/BaseManager.cpp
@@ -1,6 +1,7 @@
 #include "BaseManager.h"
 
 using namespace BWAPI;
+using namespace Filter;
 
 BaseManager::BaseManager()
 {
@@ -10,4 +11,44 @@ BaseManager& BaseManager::Instance()
 {
 	static BaseManager instance;
 	return instance;
+}
+
+void BaseManager::ManageBases(Unit u)
+{
+	hatcheries.insert(u);
+	if ((Broodwar->self()->minerals() >= UnitTypes::Zerg_Drone.mineralPrice()) && (WorkerManager::Instance().bo[WorkerManager::Instance().indx] == UnitTypes::Zerg_Drone)) {
+		if (!u->getLarva().empty()) {
+			u->train(UnitTypes::Zerg_Drone);
+			WorkerManager::Instance().indx++;
+		}
+	}
+
+	if ((Broodwar->self()->minerals() >= UnitTypes::Zerg_Overlord.mineralPrice()) && ((WorkerManager::Instance().bo[WorkerManager::Instance().indx] == UnitTypes::Zerg_Overlord) || ((WorkerManager::Instance().indx >= WorkerManager::Instance().bo.size()) && (Broodwar->self()->supplyTotal() - Broodwar->self()->supplyUsed() <= 1)))) {
+		if (!u->getLarva().empty()) {
+			u->train(UnitTypes::Zerg_Overlord);
+			WorkerManager::Instance().indx++;
+		}
+	}
+
+	for (auto& u2 : Broodwar->self()->getUnits()) {
+		if ((IsWorker)(u2))
+			workerList.insert(u2);
+	}
+
+	if ((workerList.size() < (hatcheries.size() * 3)) && (Broodwar->self()->minerals() >= UnitTypes::Zerg_Drone.mineralPrice())) {
+		if (!u->getLarva().empty()) {
+			u->train(UnitTypes::Zerg_Drone);
+			DebugMessenger::Instance() << "droning up from " << workerList.size() << " to " << (hatcheries.size() * 3) << std::endl;
+		}
+	}
+
+	if ((WorkerManager::Instance().poolready) && (Broodwar->self()->minerals() >= UnitTypes::Zerg_Zergling.mineralPrice())
+	    && (Broodwar->self()->supplyTotal() - Broodwar->self()->supplyUsed() > 0)
+	    && ((WorkerManager::Instance().bo[WorkerManager::Instance().indx] == UnitTypes::Zerg_Zergling)
+	        || (WorkerManager::Instance().indx >= WorkerManager::Instance().bo.size()))) {
+		if (!u->getLarva().empty()) {
+			u->train(UnitTypes::Zerg_Zergling);
+			WorkerManager::Instance().indx++;
+		}
+	}
 }
--- PeregrineBot/Source/BaseManager.h
+++ PeregrineBot/Source/BaseManager.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "Common.h"
+#include "WorkerManager.h"
 
 class BaseManager {
 	BaseManager();
 
 public:
 	static BaseManager& Instance();
+	void ManageBases(BWAPI::Unit u);
+	std::set<BWAPI::Unit> hatcheries;
+	std::set<BWAPI::Unit> workerList;
 };
