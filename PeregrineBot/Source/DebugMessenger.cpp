9903580c9a0935364edf8c479511dc5a5f13b4d0
Sam Jones <s7jones@users.noreply.github.com>
2018-02-02 22:11:50 +0000

    Debug can now be set from JSON config file.

--- PeregrineBot/Source/DebugMessenger.cpp
+++ PeregrineBot/Source/DebugMessenger.cpp
@@ -1,4 +1,11 @@
 #include "DebugMessenger.h"
+//#include <cpptoml.h>
+#include <rapidjson\document.h>
+#include <rapidjson\filereadstream.h>
+#include <rapidjson\rapidjson.h>
+#include <rapidjson\reader.h>
+
+using namespace rapidjson;
 
 DebugMessenger::DebugMessenger()
 {
@@ -14,9 +21,54 @@ DebugMessenger::~DebugMessenger()
 {
 }
 
-void DebugMessenger::SetDebugMode(bool debug)
+void DebugMessenger::Setup(bool debug_flag_define)
 {
-	debug_enabled = debug;
+	/*
+	// TOML snippet for later?
+	boost::filesystem::path config_file_path("./bwapi-data/read/config.toml");
+
+	bool debug_flag_toml = false;
+
+	try {
+		auto config = cpptoml::parse_file(config_file_path.string());
+
+		debug_flag_toml = config->get_as<bool>("debug").value_or(false); // val is bool value in toml or false
+	}
+	catch (const cpptoml::parse_exception& e) {
+		BWAPI::Broodwar << "Config file not read" << std::endl;
+	}
+
+	if (debug_flag_toml || debug_flag_define) {
+		debug_enabled = true;
+	}
+	*/
+
+	boost::filesystem::path config_file_path("./bwapi-data/read/PeregrineConfig.json");
+
+	//FILE* fp = fopen_s(config_file_path.string(), "rb"); // rapidjson recommended fopen, but compiler recommends "safe" variant
+	FILE* fp;
+	const wchar_t* mode = L"rb";
+	errno_t err = _wfopen_s(&fp, config_file_path.c_str(), mode);
+
+	char readBuffer[65536];
+	FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+	Document d;
+	d.ParseStream(is);
+
+	fclose(fp);
+
+	bool debug_flag_json = false;
+
+	if (d.HasMember("debug")) {
+		if (d["debug"].IsBool()) {
+			debug_flag_json = d["debug"].GetBool();
+		}
+	}
+
+	if (debug_flag_json || debug_flag_define) {
+		debug_enabled = true;
+	}
 }
 
 DebugMessenger& DebugMessenger::operator<<(BWAPI::GameWrapper::ostream_manipulator fn)
