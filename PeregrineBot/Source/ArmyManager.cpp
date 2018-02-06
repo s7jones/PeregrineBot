c8a7abdb8a6871f3d2305d93063287083eac44ed
Sam Jones <s7jones@users.noreply.github.com>
2018-02-06 01:07:20 +0000

    previous solution seemed to work because isIdle would be false after a command in same frame.
    others have suggested this was due to latcom (latency compensation). to be sure the bots behaviour is consistent I've added a more explicit check on the targeting.
    
    i've also brought over some more variables from InformationManager for future work.

--- PeregrineBot/Source/ArmyManager.cpp
+++ PeregrineBot/Source/ArmyManager.cpp
@@ -58,20 +58,39 @@ void ArmyManager::ZerglingScout(Unit u)
 
 void ArmyManager::ZerglingAttack(Unit u)
 {
-	auto enemyBase            = InformationManager::Instance().enemyBase;
-	auto isEnemyBaseReached   = InformationManager::Instance().isEnemyBaseReached;
-	auto enemyRace            = InformationManager::Instance().enemyRace;
-	auto isEnemyBaseDestroyed = InformationManager::Instance().isEnemyBaseDestroyed;
-	auto scoutingOptions      = InformationManager::Instance().scoutingOptions;
-	auto scoutedPositions     = InformationManager::Instance().scoutedPositions;
-	auto unscoutedPositions   = InformationManager::Instance().unscoutedPositions;
+	auto enemyBase                       = InformationManager::Instance().enemyBase;
+	auto enemyRace                       = InformationManager::Instance().enemyRace;
+	auto scoutingOptions                 = InformationManager::Instance().scoutingOptions;
+	auto scoutedPositions                = InformationManager::Instance().scoutedPositions;
+	auto unscoutedPositions              = InformationManager::Instance().unscoutedPositions;
+	auto isEnemyBaseFromOverlordSpotting = InformationManager::Instance().isEnemyBaseFromOverlordSpotting;
+	auto isEnemyBaseDeduced              = InformationManager::Instance().isEnemyBaseDeduced;
+	auto isEnemyBaseReached              = InformationManager::Instance().isEnemyBaseReached;
+	auto isEnemyBaseFound                = InformationManager::Instance().isEnemyBaseFound;
+	auto isEnemyBaseDestroyed            = InformationManager::Instance().isEnemyBaseDestroyed;
 	if ((enemyBase.x != 0) && (enemyBase.y != 0)) {
 		if ((!isEnemyBaseReached) && (BWTA::getRegion(u->getPosition()) == BWTA::getRegion(enemyBase))) {
 			InformationManager::Instance().isEnemyBaseReached = true;
 			DebugMessenger::Instance() << "reach enemy base: " << Broodwar->getFrameCount() << "F" << std::endl;
 		}
 	}
-	//Broodwar->setLocalSpeed(23);
+
+	/* ## isIdle calling false after previous attack order in frame
+	s7jones / Peregrine - Today at 12:51 AM
+		so basically if I call Unit->attack(something); then next line
+		Unit->isIdle();
+
+		then isIdle should return false?
+	jaj22 - Today at 12:51 AM
+		yeah, I just checked
+	++N00byEdge - Today at 12:51 AM
+		yes, if you're using latcom
+	s7jones / Peregrine - Today at 12:51 AM
+		that's crazy, I really don't have good intuitions about BWAPI and the engine
+	*/
+
+	bool priorityTarget = false;
+
 	if (enemyRace == Races::Protoss) { // if protoss - enemy then pylon then worker
 		Unit enemy = u->getClosestUnit(
 		    IsEnemy && (GetType == UnitTypes::Zerg_Zergling || GetType == UnitTypes::Terran_Marine || GetType == UnitTypes::Protoss_Zealot || GetType == UnitTypes::Zerg_Sunken_Colony || GetType == UnitTypes::Terran_Bunker || GetType == UnitTypes::Protoss_Photon_Cannon));
@@ -91,6 +110,7 @@ void ArmyManager::ZerglingAttack(Unit u)
 		}
 		if (target) {
 			OrderManager::Instance().Attack(u, target);
+			priorityTarget = true;
 		}
 	}      // end if protoss
 	else { // not protoss - enemy and worker then supply
@@ -109,6 +129,7 @@ void ArmyManager::ZerglingAttack(Unit u)
 		}
 		if (target) {
 			OrderManager::Instance().Attack(u, target);
+			priorityTarget = true;
 		}
 	}
 
@@ -118,71 +139,73 @@ void ArmyManager::ZerglingAttack(Unit u)
 	//	Broodwar << "attacking neutral" << std::endl;
 	//}
 
-	if (u->isIdle()) {
-		Unit enemy_atall = u->getClosestUnit(IsEnemy);
-		if (enemy_atall) {
-			OrderManager::Instance().Attack(u, enemy_atall);
-		} else {
-			if ((enemyBase.x != 0) && (enemyBase.y != 0)) {
-				if (!isEnemyBaseDestroyed) {
-					if (!Broodwar->isVisible(TilePosition(enemyBase))) {
-						OrderManager::Instance().Attack(u, enemyBase);
-					} else if (!Broodwar->getUnitsOnTile(TilePosition(enemyBase), IsEnemy && IsVisible && Exists && IsBuilding && !IsLifted).empty()) {
-						OrderManager::Instance().Attack(u, enemyBase);
-					} else {
+	if (!priorityTarget) {
+		if (u->isIdle()) {
+			Unit enemy_atall = u->getClosestUnit(IsEnemy);
+			if (enemy_atall) {
+				OrderManager::Instance().Attack(u, enemy_atall);
+			} else {
+				if ((enemyBase.x != 0) && (enemyBase.y != 0)) {
+					if (!isEnemyBaseDestroyed) {
+						if (!Broodwar->isVisible(TilePosition(enemyBase))) {
+							OrderManager::Instance().Attack(u, enemyBase);
+						} else if (!Broodwar->getUnitsOnTile(TilePosition(enemyBase), IsEnemy && IsVisible && Exists && IsBuilding && !IsLifted).empty()) {
+							OrderManager::Instance().Attack(u, enemyBase);
+						} else {
+							ZerglingScout(u);
+						}
+					} // havent destroyed enemy base
+					else {
 						ZerglingScout(u);
 					}
-				} // havent destroyed enemy base
+				} // no enemy base
 				else {
-					ZerglingScout(u);
-				}
-			} // no enemy base
-			else {
-				if (Broodwar->getStartLocations().size() == 4) { // map size is 4, use new scouting
-					auto tp1                       = scoutingOptions.begin()->startToP1ToP2[1];
-					auto p1                        = GetBasePos(tp1);
-					const bool firstOptionScouted  = scoutedPositions.find(p1) != scoutedPositions.end();
-					auto tp2                       = scoutingOptions.begin()->startToP1ToP2[2];
-					auto p2                        = GetBasePos(tp2);
-					const bool secondOptionScouted = scoutedPositions.find(p2) != scoutedPositions.end();
-					if (!firstOptionScouted) {
-						OrderManager::Instance().Move(u, p1);
-						DebugMessenger::Instance() << " moving to 1: " << tp1.x << "," << tp1.y << "TP" << std::endl;
-					} else if (!secondOptionScouted) {
-						OrderManager::Instance().Move(u, p2);
-						DebugMessenger::Instance() << " moving to 2: " << tp2.x << "," << tp2.y << "TP" << std::endl;
-					} else {
+					if (Broodwar->getStartLocations().size() == 4) { // map size is 4, use new scouting
+						auto tp1                       = scoutingOptions.begin()->startToP1ToP2[1];
+						auto p1                        = GetBasePos(tp1);
+						const bool firstOptionScouted  = scoutedPositions.find(p1) != scoutedPositions.end();
+						auto tp2                       = scoutingOptions.begin()->startToP1ToP2[2];
+						auto p2                        = GetBasePos(tp2);
+						const bool secondOptionScouted = scoutedPositions.find(p2) != scoutedPositions.end();
+						if (!firstOptionScouted) {
+							OrderManager::Instance().Move(u, p1);
+							DebugMessenger::Instance() << " moving to 1: " << tp1.x << "," << tp1.y << "TP" << std::endl;
+						} else if (!secondOptionScouted) {
+							OrderManager::Instance().Move(u, p2);
+							DebugMessenger::Instance() << " moving to 2: " << tp2.x << "," << tp2.y << "TP" << std::endl;
+						} else {
+							auto p = *unscoutedPositions.begin();
+							OrderManager::Instance().Move(u, p);
+							auto tp3 = (TilePosition)p;
+							DebugMessenger::Instance() << " moving to else: " << tp3.x << "," << tp3.y << "TP" << std::endl;
+						}
+					} else { // map size isn't 4, so use old scouting
 						auto p = *unscoutedPositions.begin();
 						OrderManager::Instance().Move(u, p);
-						auto tp3 = (TilePosition)p;
-						DebugMessenger::Instance() << " moving to else: " << tp3.x << "," << tp3.y << "TP" << std::endl;
+						//move(u, (*unscoutedOtherPositions.begin()));
+						/*for (auto p : unscoutedOtherPositions) {
+							u->move(p, true);
+							}*/
 					}
-				} else { // map size isn't 4, so use old scouting
+				}
+			}
+		} // end if idle
+		else if (u->isMoving()) {
+			UnitCommand lastCmd = u->getLastCommand();
+			if (lastCmd.getType() == UnitCommandTypes::Move) {
+				Position targetPos = lastCmd.getTargetPosition();
+				//if (unscoutedOtherPositions.find(targetPos) == unscoutedOtherPositions.end()) {
+				if ((unscoutedPositions.count(targetPos) == 0) && (!unscoutedPositions.empty())) {
+					DebugMessenger::Instance() << "recalculate scouting" << std::endl;
+
+					u->stop();
 					auto p = *unscoutedPositions.begin();
 					OrderManager::Instance().Move(u, p);
-					//move(u, (*unscoutedOtherPositions.begin()));
 					/*for (auto p : unscoutedOtherPositions) {
 						u->move(p, true);
 						}*/
 				}
 			}
-		}
-	} // end if idle
-	else if (u->isMoving()) {
-		UnitCommand lastCmd = u->getLastCommand();
-		if (lastCmd.getType() == UnitCommandTypes::Move) {
-			Position targetPos = lastCmd.getTargetPosition();
-			//if (unscoutedOtherPositions.find(targetPos) == unscoutedOtherPositions.end()) {
-			if ((unscoutedPositions.count(targetPos) == 0) && (!unscoutedPositions.empty())) {
-				DebugMessenger::Instance() << "recalculate scouting" << std::endl;
-
-				u->stop();
-				auto p = *unscoutedPositions.begin();
-				OrderManager::Instance().Move(u, p);
-				/*for (auto p : unscoutedOtherPositions) {
-					u->move(p, true);
-					}*/
-			}
-		}
-	} // end if moving
+		} // end if moving
+	}
 }
