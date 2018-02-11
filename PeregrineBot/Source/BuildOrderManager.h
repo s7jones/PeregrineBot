6d3308132fdfc64b35d4a34236c1a9895b740643
Sam Jones <s7jones@users.noreply.github.com>
2018-02-11 19:49:52 +0000

    clang format plus simplify build order vector initialization

--- PeregrineBot/Source/BuildOrderManager.h
+++ PeregrineBot/Source/BuildOrderManager.h
@@ -10,22 +10,24 @@ public:
 	// https://msdn.microsoft.com/en-us/library/dn793970.aspx
 	// Seems to be Error C2664 in VS2017?
 	typedef std::vector<BWAPI::UnitType> Vector;
-	const Vector bo = Vector{ BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Drone),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Spawning_Pool),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Drone),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Drone),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Zergling),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Zergling),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Zergling),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Overlord),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Zergling),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Zergling),
-		BWAPI::UnitType(BWAPI::UnitTypes::Zerg_Zergling) };
+	const Vector bo = {
+		BWAPI::UnitTypes::Zerg_Drone,
+		BWAPI::UnitTypes::Zerg_Spawning_Pool,
+		BWAPI::UnitTypes::Zerg_Drone,
+		BWAPI::UnitTypes::Zerg_Drone,
+		BWAPI::UnitTypes::Zerg_Zergling,
+		BWAPI::UnitTypes::Zerg_Zergling,
+		BWAPI::UnitTypes::Zerg_Zergling,
+		BWAPI::UnitTypes::Zerg_Overlord,
+		BWAPI::UnitTypes::Zerg_Zergling,
+		BWAPI::UnitTypes::Zerg_Zergling,
+		BWAPI::UnitTypes::Zerg_Zergling
+	};
 
 	Vector::const_iterator boIndex = bo.begin();
-	bool pool = false;
-	bool poolready = false;
-	bool buildOrderComplete = false;
+	bool pool                      = false;
+	bool poolready                 = false;
+	bool buildOrderComplete        = false;
 
 private:
 	BuildOrderManager();
