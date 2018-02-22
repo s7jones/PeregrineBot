567e9ab36a133dae4f2456d8a15de292f820b766
Sam Jones <s7jones@users.noreply.github.com>
2018-02-22 22:47:54 +0000

    replace virtual with override
    (cherry picked from commit 5c2eac37e88d5bcced4588f4003abb48364fe088)

--- PeregrineBot/Source/PeregrineBot.h
+++ PeregrineBot/Source/PeregrineBot.h
@@ -6,23 +6,23 @@
 class PeregrineBot : public BWAPI::AIModule {
 public:
 	// Virtual functions for callbacks, leave these as they are.
-	virtual void onStart();
-	virtual void onEnd(bool isWinner);
-	virtual void onFrame();
-	virtual void onSendText(std::string text);
-	virtual void onReceiveText(BWAPI::Player player, std::string text);
-	virtual void onPlayerLeft(BWAPI::Player player);
-	virtual void onNukeDetect(BWAPI::Position target);
-	virtual void onUnitDiscover(BWAPI::Unit unit);
-	virtual void onUnitEvade(BWAPI::Unit unit);
-	virtual void onUnitShow(BWAPI::Unit unit);
-	virtual void onUnitHide(BWAPI::Unit unit);
-	virtual void onUnitCreate(BWAPI::Unit unit);
-	virtual void onUnitDestroy(BWAPI::Unit unit);
-	virtual void onUnitMorph(BWAPI::Unit unit);
-	virtual void onUnitRenegade(BWAPI::Unit unit);
-	virtual void onSaveGame(std::string gameName);
-	virtual void onUnitComplete(BWAPI::Unit unit);
+	void onStart() override;
+	void onEnd(bool isWinner) override;
+	void onFrame() override;
+	void onSendText(std::string text) override;
+	void onReceiveText(BWAPI::Player player, std::string text) override;
+	void onPlayerLeft(BWAPI::Player player) override;
+	void onNukeDetect(BWAPI::Position target) override;
+	void onUnitDiscover(BWAPI::Unit unit) override;
+	void onUnitEvade(BWAPI::Unit unit) override;
+	void onUnitShow(BWAPI::Unit unit) override;
+	void onUnitHide(BWAPI::Unit unit) override;
+	void onUnitCreate(BWAPI::Unit unit) override;
+	void onUnitDestroy(BWAPI::Unit unit) override;
+	void onUnitMorph(BWAPI::Unit unit) override;
+	void onUnitRenegade(BWAPI::Unit unit) override;
+	void onSaveGame(std::string gameName) override;
+	void onUnitComplete(BWAPI::Unit unit) override;
 	// Everything below this line is safe to modify.
 
 	const std::string version = "v4";
